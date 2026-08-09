5441e8f43078c96a4dc6d0187fcd02dd6dafa761
Teddy Mazuri <teddymazuri@gmail.com>
2026-08-10 02:07:46 +0300

    Configure GitHub Pages automated build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://teddymazuri.github.io',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
