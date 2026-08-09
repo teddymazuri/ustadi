07f0100640af9edd2b912d40d945fe4195283690
Teddy Mazuri <teddymazuri@gmail.com>
2026-08-10 02:40:06 +0300

    Set correct base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://teddymazuri.github.io',
-  base: '/',
+  base: '/ustadi',
   vite: {
     plugins: [tailwindcss()]
   }
