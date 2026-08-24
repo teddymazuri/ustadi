17f2257ad286d54b6e4ec1ed54f54b53ce290641
Teddy Mazuri <teddymazuri@gmail.com>
2026-08-24 15:45:30 +0300

    Sitemap integration in the astro.config.mjs file

--- astro.config-bk2.mjs
+++ astro.config-bk2.mjs
@@ -1,15 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: process.env.NETLIFY 
-    ? 'https://ustadi-org.netlify.app'  // ← Keep this for now
+    ? 'https://ustadi-org.netlify.app' 
     : 'https://teddymazuri.github.io',
   base: process.env.NETLIFY ? '/' : '/ustadi/',
   vite: {
     plugins: [tailwindcss()]
-  },
-  integrations: [sitemap()],  // ← Just add this
+  }
 });
